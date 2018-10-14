e32826179aa377ea32416eaad153f3c71206ae57
Roslan Logan <roslanlogan94@gmail.com>
2018-10-14 23:05:10 +0800

    changes others

--- src/module/purchase/purchase.component.ts
+++ src/module/purchase/purchase.component.ts
@@ -17,6 +17,7 @@ export class PurchaseComponent implements OnInit {
   constructor(private router: Router) { }
   searchText: string;
   tableData = this.addPlateNum(DIAMOND_DATA);
+  selected: any;
 
   filterIt(arr, searchKey) {
     return arr.filter((obj) => {
@@ -38,7 +39,6 @@ export class PurchaseComponent implements OnInit {
   }
 
   selectedType(type) {
-    console.log(type.target.value)
     if (type.target.value == 1) {
       this.tableData = this.addPlateNum(DIAMOND_DATA);
     } else if (type.target.value == 2) {
@@ -49,21 +49,42 @@ export class PurchaseComponent implements OnInit {
       this.tableData = this.addPlateNum(SILVER_DATA);
     } else if (type.target.value == 5) {
       this.tableData = this.addPlateNum(SUPER_BRONZE_DATA);
-    } else if (type.target.value == 5) {
+    } else if (type.target.value == 6) {
       this.tableData = this.addPlateNum(BRONZE_DATA);
     }
+    this.selected = type.target.value
   }
 
   addPlateNum(data) {
-    console.log(data)
-    data.forEach(data => {
-      if (data.plateNum.indexOf("IM") == -1) {
-        data.plateNum = "IM" + data.plateNum;
-      }
-    })
+    console.log(data.length)
+    if (data.length > 0) {
+      data.forEach(data => {
+        if (data.plateNum.indexOf("IM") == -1) {
+          data.plateNum = "IM" + data.plateNum;
+        }
+      })
+    } else {
+      data = null
+    }
     return data;
   }
   proceedBooking(plateNumber) {
     window.open('https://drive.google.com/uc?export=download&id=1yomOb78YK_C5zP1Och8hEo5HP2PHZluv', '_blank');
+  }
+
+  getPriceIfNotExist(id) {
+    if (id == 1) {
+      return "RM 100,000.00";
+    } else if (id == 2) {
+      return "RM 50,000.00";
+    } else if (id == 3) {
+      return "RM 10000.00";
+    } else if (id == 4) {
+      return "RM 5000.00";
+    } else if (id == 5) {
+      return "RM 3000.00";
+    } else if (id == 6) {
+      return "RM 1500.00";
+    }
   }
 }
